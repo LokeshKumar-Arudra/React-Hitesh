57dee1648657f40c177b952a150f349756365cf6
Lokesh Arudra <larudra@osius.com>
2025-11-18 20:04:48 +0530

    First vite react project

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,12 @@
+import Youtube from "./Youtube.jsx";
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Youtube/>
+    </>
+  )
+}
+
+export default App
---
+++ 01basicvite/src/Youtube.jsx
@@ -0,0 +1,7 @@
+function Youtube() {
+    return (
+        <h1>Return from Youtube</h1>
+    )
+}
+
+export default Youtube
